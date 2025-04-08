f6fac69e03d48fe04cc48c15aa29f78e8d76fdf9
surenfernand <141896367+surenfernand@users.noreply.github.com>
2025-04-08 11:55:07 +0530

    first_commit

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+import { CardGridSkeleton } from "@/components/card-grid-skeleton";
+import { DeployButton } from "@/components/deploy-button";
+import { SearchBox } from "@/components/search-box";
+import { SuspendedImageSearch } from "@/components/suspended-image-search";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ q?: string }>;
+}) {
+  const query = (await searchParams).q;
+  return (
+    <main className="p-8 space-y-4">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="font-semibold text-2xl">Semantic Search</h1>
+        </div>
+        <DeployButton />
+      </div>
+      <div>
+        <p>
+          This demo showcases how to use the{" "}
+          <Link
+            href="https://sdk.vercel.ai/docs"
+            className="text-blue-600 hover:underline"
+            target="_blank"
+          >
+            AI SDK
+          </Link>{" "}
+          to build semantic search applications. Try searching for something
+          semantically, like &quot;tasty food&quot;.
+        </p>
+      </div>
+      <div className="">
+        <div className="pt-2">
+          <SearchBox query={query} />
+        </div>
+        <Suspense fallback={<CardGridSkeleton />} key={query}>
+          <SuspendedImageSearch query={query} />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
